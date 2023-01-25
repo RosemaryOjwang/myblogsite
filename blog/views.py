698faec31a30d473ff3437a091d3a0af638b8260
Rosemary Ojwang <rosemaryojwang26@gmail.com>
2023-01-26 00:16:03 +0300

    build canonical URL for post objects

--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,7 @@ from django.http import Http404
 # Create your views here.
 def post_list(request):
      posts = Post.published.all()
+
      return render(request,
                   'blog/post/list.html',
                    {'posts': posts})
