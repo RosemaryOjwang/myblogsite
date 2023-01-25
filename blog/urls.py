eba185f40265c64ee635149b0fe2b65b845432ff
Rosemary Ojwang <rosemaryojwang26@gmail.com>
2023-01-25 15:48:04 +0300

    create static/blog.css and fix errors related to it

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 app_name = 'blog'
 
-url_patterns = [
+urlpatterns = [
+    #post views
     path('', views.post_list, name='post_list'),
     path('<int:id>/', views.post_detail, name='post_detail'), 
 ]
--- blog/views.py
+++ blog/views.py
@@ -4,22 +4,17 @@ from django.http import Http404
 
 # Create your views here.
 def post_list(request):
-    posts = get_object_or_404(Post,
-                              id=id,
-                              status=Post.Status.PUBLISHED)
-
-    return render(request,
+     posts = Post.published.all()
+     return render(request,
                   'blog/post/list.html',
-                  {'posts': posts}) 
+                   {'posts': posts})
 
 def post_detail(request, id):
-    try:
-        post = Post.published.get(id=id)
-    except Post.DoesNotExist:
-        raise Http404('No post found.')
+    post = get_object_or_404(Post,
+                                 id=id,
+                                  status=Post.Status.PUBLISHED)
 
     return render(request,
                  'blog/post/detail.html',
-                 {'post': post}
-                )
+                 {'post': post})
     
