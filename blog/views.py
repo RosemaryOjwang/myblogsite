6f308225f37cec7ea848f9d81189e432470f688a
Rosemary Ojwang <rosemaryojwang26@gmail.com>
2023-01-27 03:04:36 +0300

    complete posts sharing by emails functionality

--- blog/views.py
+++ blog/views.py
@@ -48,29 +48,27 @@ def post_detail(request, year, month, day, post):
                 {'post': post})
 
 def post_share(request, post_id):
-    #retrieve post by id
+     # Retrieve post by id
     post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
     sent = False
-
+ 
     if request.method == 'POST':
-        #Form was submitted
+    # Form was submitted
         form = EmailPostForm(request.POST)
         if form.is_valid():
-            #Form fields passed validation
+            # Form fields passed validation
             cd = form.cleaned_data
-            post_url = request.build_absolute_url(
+            post_url = request.build_absolute_uri(
                 post.get_absolute_url())
             subject = f"{cd['name']} recommends you read " \
-                f"{post.title}"
+                      f"{post.title}"
             message = f"Read {post.title} at {post_url}\n\n" \
-                f"{cd['name']}\'s comments: {cd['comments']}"
-            send_mail(subject, message, 'rosemaryojwang26@gmail.com',
+                      f"{cd['name']}\'s comments: {cd['comments']}"
+            send_mail(subject, message, 'your_account@gmail.com',
                       [cd['to']])
             sent = True
-
-        else:
-            form = EmailPostForm()
-
-        return render(request, 'blog/post/share.html', {'post': post,
-                                                        'form': form,
-                                                        'sent': sent})
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post': post,
+                                                    'form': form,
+                                                    'sent': sent})
