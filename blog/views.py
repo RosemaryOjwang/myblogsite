a3575b891a2af8503ae692b25929f608776fa6d0
Rosemary Ojwang <rosemaryojwang26@gmail.com>
2023-01-27 23:00:55 +0300

    implement the view to manage submission of comments and their[C corresponding URL

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post
+from .models import Post, Comment
 from django.http import Http404
 from django.views.generic import ListView
-from .forms import EmailPostForm
+from .forms import EmailPostForm, CommentForm
+from django.views.decorators.http import require_POST
 from django.core.mail import send_mail
 from django.core.paginator import Paginator, EmptyPage,\
                                   PageNotAnInteger
@@ -72,3 +73,22 @@ def post_share(request, post_id):
     return render(request, 'blog/post/share.html', {'post': post,
                                                     'form': form,
                                                     'sent': sent})
+
+    @require_POST
+    def post_comment(request, post_id):
+        post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+        comment = None
+        #A comment has been posted
+        form = CommentForm(data=request.POST)
+        if form.is_valid():
+            #Create a comment object without saving it to the database
+            comment = form.save(commit=False)
+            #Assign the post to the comment
+            comment.post = post
+            #Save the comment to the database
+            comment.save()
+        return render(request, 'blog/post/comment.html',
+                      {'post': post,
+                      'form': form,
+                      'comment': comment})
+
