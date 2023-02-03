3ff4ecc7d7b6859a2b878102a1c375a75038e080
Rosemary Ojwang <rosemaryojwang26@gmail.com>
2023-02-04 00:33:25 +0300

    add sitemap to the site

--- myblogsite/settings.py
+++ myblogsite/settings.py
@@ -31,6 +31,7 @@ SECRET_KEY = env("SECRET_KEY")
 DEBUG = True
 
 ALLOWED_HOSTS = []
+SITE_ID = 1
 
 
 # Application definition
@@ -44,6 +45,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
     'taggit',
+    'django.contrib.sites',
+    'django.contrib.sitemaps',
 ]
 
 MIDDLEWARE = [
